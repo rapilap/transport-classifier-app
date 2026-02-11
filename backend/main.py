d31bcfcb61d4ee8313ac5f8bcae40f4d6018f20e
rapilap <raffyabdillah03@gmail.com>
2026-02-11 07:49:43 +0700

    refactor preparing for deploy

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Vehicle Classification API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://*.vercel.app", "*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
